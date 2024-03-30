da7382b6d0216a0c022b1df7ab2fcbc850d7a920
Nethusan Chandrakumar <nethusan.chandrakumar@haldor.se>
2024-03-30 13:50:02 +0100

    updated

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MediaPickerExampleProject
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
